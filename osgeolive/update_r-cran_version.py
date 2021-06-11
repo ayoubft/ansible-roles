cb05a05f2e81cf01c3de8b746aa6cc62189d5b36
Ayoub FATIHI <ayoubfatihi1999@gmail.com>
2021-06-11 11:20:40 +0200

    Added automated update for r-cran proxy package

--- osgeolive/update_r-cran_version.py
+++ osgeolive/update_r-cran_version.py
@@ -19,21 +19,27 @@ res = listFD(url, ext)
 print("STATUS: fetching the requrired data")
 res_e_ver = ""
 res_class_int = ""
+res_proxy = ""
 for file in res:
     if re.match(r"^https://cran\.r-project\.org/src/contrib//e[0-9].*\.tar\.gz$", file):
         res_e_ver = file.split(r"//")[2]
     elif re.match(r"https://cran\.r-project\.org/src/contrib//classInt_.*\.tar\.gz", file):
         res_class_int = file.split(r"//")[2]
+    elif re.match(r"https://cran\.r-project\.org/src/contrib//proxy_.*\.tar\.gz", file):
+        res_proxy = file.split(r"//")[2]
 
-print(f"STATUS:\n\t{res_e_ver}\n\t{res_class_int}")
+print(f"STATUS:\n\t{res_e_ver}\n\t{res_class_int}\n\t{res_proxy}")
 
 old_e_ver = "      - e1071_1.7-2.tar.gz"
 old_classInt = "      - classInt_0.4-1.tar.gz"
+old_proxy = "      - proxy_0.4-25.tar.gz"
 new_e_ver = f"      - {res_e_ver}"
 new_classInt = f"      - {res_class_int}"
+new_proxy = f"      - {res_proxy}"
 file_path = r"dependencies/tasks/r-cran.yml"
 
 os.system(f'sed --in-place "s/{old_e_ver}/{new_e_ver}/g" "{file_path}"')
 os.system(f'sed --in-place "s/{old_classInt}/{new_classInt}/g" "{file_path}"')
+os.system(f'sed --in-place "s/{old_proxy}/{new_proxy}/g" "{file_path}"')
 
 print("STATUS: successfully updated the versions :)")
